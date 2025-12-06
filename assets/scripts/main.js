6022131260fa6199cb1f07a025d1964f237be883
FakhriB <fakhri-azmpa201@code.edu.az>
2025-12-07 00:04:07 +0400

    fixing image error

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,38 +1,33 @@
-fetch("https://api.tvmaze.com/shows?&select=key1,key2,key3").then(res=>res.json()).then(data=>{
+fetch("https://api.tvmaze.com/shows")
+  .then(function(res) {
+    return res.json();
+  })
+  .then(function(data) {
+    const cardsDiv = document.querySelector(".cards");
+    cardsDiv.innerHTML = "";
 
-const cardsDiv = document.querySelector(".cards");
- 
-data.forEach(element => {
-    cardsDiv.innerHTML += `
-       
-    <div class="col">
-<div class="card" style="width: 18rem;">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
+    data.forEach(function(show) {
+      var imgSrc;
+      if (show.image && show.image.medium) {
+        imgSrc = show.image.medium;
+      } else {
+        imgSrc = 'https://via.placeholder.com/210x295?text=No+Image';
+      }
 
-    `
-});
-console.log(data)
-}).catch(err=>{
-    console.log(err);  
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      cardsDiv.innerHTML += `
+        <div class="col mb-4">
+          <div class="card" style="width: 18rem;">
+            <img src="${imgSrc}" class="card-img-top" alt="${show.name}">
+            <div class="card-body">
+              <h5 class="card-title">${show.name}</h5>
+              <p class="card-text">${show.summary ? show.summary.replace(/<[^>]+>/g, '').substring(0, 100) + '...' : 'Açıqlama yoxdur'}</p>
+              <a href="detail.html?id=${show.id}" class="btn btn-primary">Detala bax</a>
+            </div>
+          </div>
+        </div>
+      `;
+    });
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
